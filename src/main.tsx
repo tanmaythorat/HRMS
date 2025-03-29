79fcd4f36ebefd0495dae9dfe168e07f6898cd01
tanmaythorat <tanmaythorat9@gmail.com>
2025-03-29 19:40:43 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HashRouter>
     <App />
-  </StrictMode>
+  </HashRouter>
 );
