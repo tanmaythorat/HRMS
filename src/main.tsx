58df912a8a51789889ddcc0947e3d3e0cd81536d
tanmaythorat <tanmaythorat9@gmail.com>
2025-03-29 19:53:07 +0530

    Clean cache and fix deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { HashRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/HRMS/',  // 👈 Replace with your actual repo name
   plugins: [react()],
-  base: '/HRMS/', // 👈 Use your GitHub repo name here
 });
